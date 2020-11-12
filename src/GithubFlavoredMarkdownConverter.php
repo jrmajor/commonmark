34614735a1fb95bcc917863dd53f42672a64944e
Colin O'Dell <colinodell@gmail.com>
2020-11-12 10:42:09 -0500

    Add new base MarkdownConverter class; extend from that

--- src/GithubFlavoredMarkdownConverter.php
+++ src/GithubFlavoredMarkdownConverter.php
@@ -14,12 +14,15 @@ declare(strict_types=1);
 namespace League\CommonMark;
 
 use League\CommonMark\Environment\Environment;
+use League\CommonMark\Environment\EnvironmentBuilderInterface;
 use League\CommonMark\Environment\EnvironmentInterface;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\GithubFlavoredMarkdownExtension;
 
 /**
  * Converts Github Flavored Markdown to HTML.
  */
-class GithubFlavoredMarkdownConverter extends CommonMarkConverter
+class GithubFlavoredMarkdownConverter extends MarkdownConverter
 {
     /**
      * Create a new commonmark converter instance.
@@ -28,10 +31,25 @@ class GithubFlavoredMarkdownConverter extends CommonMarkConverter
      */
     public function __construct(array $config = [], ?EnvironmentInterface $environment = null)
     {
-        if ($environment === null) {
-            $environment = Environment::createGFMEnvironment();
+        // Passing in an $environment is deprecated
+        if ($environment !== null) {
+            if ($config !== []) {
+                if (! ($environment instanceof EnvironmentBuilderInterface)) {
+                    throw new \RuntimeException('Unable to configure the environment as only ' . EnvironmentBuilderInterface::class . ' instances can be configured');
+                }
+
+                $environment->mergeConfig($config);
+            }
+
+            parent::__construct($environment);
+
+            return;
         }
 
-        parent::__construct($config, $environment);
+        $environment = new Environment($config);
+        $environment->addExtension(new CommonMarkCoreExtension());
+        $environment->addExtension(new GithubFlavoredMarkdownExtension());
+
+        parent::__construct($environment);
     }
 }
